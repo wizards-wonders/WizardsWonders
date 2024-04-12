7ad9e8d62bd13d948f709fd468baf269b5ddd2d0
Sam <John.Smith.Ed@outlook.com>
2024-04-12 10:31:09 +0800

    add IWizardsWonders

---
+++ contracts/interface/IWizardsWonders.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+
+interface IWizardsWonders {
+    enum RarityType {None, Boring, Fancy, SuperFancy}
+    enum carType {None, Commander, Wonder, ActionCard,Unit}
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) external returns (bool);
+    function safeMint(
+        address to_, 
+        string memory  tokenHash_,
+        string memory Series_,
+        string memory Type_,
+        string memory Rarity_,
+        string memory Faction_,
+        uint256  PointValue_,
+        bool OP_
+        ) external;
+}
