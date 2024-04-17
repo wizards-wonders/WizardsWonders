e484f3f929e85e2606b61506e26e30d52d28f293
Sam <John.Smith.Ed@outlook.com>
2024-04-17 10:16:59 +0800

    update hardhat config info

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,37 +1,101 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
-require("hardhat-deploy");
-require("hardhat-deploy-ethers");
+require("hardhat-gas-reporter");
+require('hardhat-contract-sizer');
 
-const env = require("./constants");
+require("@nomiclabs/hardhat-web3");
 
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.6.12",
-  networks: {
-    hardhat: {
-      blockGasLimit: 80000000000,
-      saveDeployments: true,
-      accounts: {
-        mnemonic: "test test test test test test test test test test test junk",
-        accountsBalance: "1000000000000000000000000000"
+  //solidity: "0.8.4",
+
+  solidity: {
+
+    
+    /*version: "0.6.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
       }
+    }*/
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+        
+      }
+    ]
+  },
+
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+
     abey: {
-      url: "https://rpc.abeychain.com",
+      url: "https://rpc.abeychain.com/",
       chainId: 179,
-      accounts: [env.PRIVATE_KEY]
-    }
+      gasPrice: 52000000000,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+
+   
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   contractSizer: {
     alphaSort: true,
-    runOnCompile: true,
     disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
   },
-  gasReporter: {
-    currency: 'USD',
-    gasPrice: 1
-  },
-  mocha: {
-    timeout: 400000
-  }
 };
